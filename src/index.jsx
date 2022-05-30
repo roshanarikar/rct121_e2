b6ac7d968255389a5694517f829f1d48d28ca97c
Roshan Arikar <roshan.g.arikar@gmail.com>
2022-05-30 16:53:56 +0530

    new update

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {RegistrationContextProvider} from "./Context/RegistrationContext"
+import {BrowserRouter} from "react-router-dom"
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
-
-
-
+ReactDOM.render(
+  <React.StrictMode>
+    <RegistrationContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RegistrationContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
